247fdc1376a100e95cd461e0eda36b896e819ba8
Travis Lilleberg <travis.lilleberg@tufts.edu>
2021-12-21 13:56:58 -0500

    add_work_to_collection_spec was throwing false positives, now it actually tests what its supposed to

--- spec/features/add_work_to_collection_spec.rb
+++ spec/features/add_work_to_collection_spec.rb
@@ -7,37 +7,27 @@ RSpec.feature 'Add a work to a collection', :clean, js: true do
     let(:work) { FactoryBot.actor_create(:image, user: admin, displays_in: ['dl']) }
     let!(:collection) { FactoryBot.create(:collection_lw) }
     let(:title) { FFaker::Book.title }
-    let(:ead_id) { 'fake_ead_id' }
 
     before { login_as admin }
 
     scenario do
-      # collection
-      visit '/dashboard'
-      click_link "Collections"
-      #      interactive_debug_session(user)
-      sleep(2)
-      click_link "New Collection"
-
-      # EAD is a form entry field when you create a new Collection
+      visit('/dashboard/collections/new')
       fill_in 'Title', with: title
-      fill_in 'EAD', with: ead_id
       click_button "Save"
-      click_link "Cancel"
-      visit("dashboard/my/collections")
-      expect(work.member_of_collections).to be_empty
+
       visit("/concern/images/#{work.id}/edit#relationships")
-      # interactive_debug_session(admin)
       expect(page).to have_content("This work is currently in these collections")
-      find('div#s2id_image_member_of_collection_ids').click
-      fill_in('s2id_autogen2_search', with: :title)
-      sleep(3)
-      # .select(collection.title.first)
-      # find('input#with_files_submit').click
-      find('a[data-behavior="add-relationship"]', match: :first).trigger('click')
-      expect(page).to have_content :title
+      find('#s2id_image_member_of_collection_ids').click
+      fill_in('s2id_autogen2_search', with: title)
+      sleep(1)
+      find('.select2-match').click
+      find('a[data-behavior="add-relationship"]', match: :first).click
+      click_button('Save changes')
+
       work.reload
-      # expect(work.member_of_collections.first.id).to eq(collection.id)
+      expect(work.member_of_collections).to eq([Collection.where(title: title).first])
+      visit("/concern/images/#{work.id}/edit#relationships")
+      expect(page).to have_content(title)
     end
   end
 end
