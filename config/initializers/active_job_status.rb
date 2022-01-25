a51a47bb20199938e852b8c0e481f8e943fb26b0
Travis Lilleberg <travis.lilleberg@tufts.edu>
2022-01-25 09:04:36 -0500

    Fixed https://tuftswork.atlassian.net/browse/TDLR-2374 by passing redis config to new RedisCacheStore

--- config/initializers/active_job_status.rb
+++ config/initializers/active_job_status.rb
@@ -1,1 +1,3 @@
-ActiveJobStatus.store = ActiveSupport::Cache::RedisCacheStore.new
+r_file = Rails.root.join('config', 'redis.yml').to_s
+r_config = YAML::load(File.open(r_file)).deep_symbolize_keys![Rails.env.to_sym]
+ActiveJobStatus.store = ActiveSupport::Cache::RedisCacheStore.new(r_config)
