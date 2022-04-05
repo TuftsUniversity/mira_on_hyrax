7f20b481f7081ee393d783b4a64905ab19eb62af
Mike Korcynski <mkorcy@gmail.com>
2022-04-05 12:20:08 -0400

    makes this a bit more flexible for docker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,12 +8,16 @@ auth = if Rails.application.config_for(:redis)["password"]
        end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://' + auth + 'localhost:6379' }
+  #config.redis = { url: 'redis://' + auth + "#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}" }
+  config.redis = { host: "#{ENV.fetch('REDIS_HOST', 'localhost')}", port: 6379, db: 0}
+
   config.failures_max_count = 5000
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://' + auth + 'localhost:6379' }
+  #config.redis = { url: 'redis://' + auth + "#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}" }
+  config.redis = { host: "#{ENV.fetch('REDIS_HOST', 'localhost')}", port: 6379, db: 0}
+  #config.redis = { url: 'redis://' + auth + "localhost:6379" }
 end
 
 Sidekiq::Web.app_url = '/dashboard'
