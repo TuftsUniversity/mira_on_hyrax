5869cd2f4a768f751825081c591c664a7bdc21af
Mike Korcynski <mkorcy@gmail.com>
2022-09-26 14:21:36 -0400

    don't require admin bind in non-prod

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # config.ldap_check_attributes = false
   # config.ldap_use_admin_to_bind = false
   # config.ldap_ad_group_check = false
-  config.ldap_use_admin_to_bind = true
+  config.ldap_use_admin_to_bind = Rails.env.production? ? true : false
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
