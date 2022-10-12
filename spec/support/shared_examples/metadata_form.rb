a67ceee765c6d055c8551f3cff2efd416c931aaf
Ryan Orlando <rynorlando@gmail.com>
2022-10-12 09:07:26 -0400

    Add missing attributes for metadata attributes and the form has Tufts terms

--- spec/support/shared_examples/metadata_form.rb
+++ spec/support/shared_examples/metadata_form.rb
@@ -8,18 +8,24 @@ shared_examples 'a form with Tufts metadata attributes' do
 
     it 'has Tufts terms' do
       expect(form.terms).to include(:abstract, :accrual_policy, :admin_start_date,
-                                    :alternative_title, :audience, :corporate_name,
-                                    :createdby, :creator_department, :date_accepted,
+                                    :alternative_title, :aspace_cuid, :audience,
+                                    :bibliographic_citation, :corporate_name,
+                                    :contributor, :creator, :createdby,
+                                    :creator_department, :date_accepted,
                                     :date_available, :date_copyrighted, :date_issued,
-                                    :displays_in, :dissertation_type, :downloadable,
-                                    :embargo_note, :end_date, :extent, :format_label,
-                                    :funder, :genre, :geographic_name, :geog_name,
-                                    :has_part, :held_by, :internal_note, :is_part_of,
-                                    :is_replaced_by, :legacy_pid, :personal_name,
-                                    :primary_date, :provenance, :qr_note, :qr_status,
-                                    :rejection_reason, :replaces, :retention_period,
-                                    :rights_holder, :rights_note, :steward,
-                                    :table_of_contents, :temporal, :tufts_license)
+                                    :date_modified, :date_uploaded, :dc_access_rights,
+                                    :description, :displays_in, :dissertation_type,
+                                    :doi, :downloadable, :embargo_note, :end_date,
+                                    :extent, :format_label, :funder, :genre,
+                                    :geographic_name, :geog_name, :has_part,
+                                    :held_by, :identifier, :internal_note, :isbn,
+                                    :is_part_of, :is_replaced_by, :language,
+                                    :legacy_pid, :oclc, :personal_name, :primary_date,
+                                    :provenance, :publisher, :qr_note, :qr_status,
+                                    :rejection_reason, :replaces, :resource_type,
+                                    :retention_period, :rights_holder, :rights_note,
+                                    :steward, :subject, :table_of_contents, :temporal,
+                                    :tufts_license)
     end
   end
 end
