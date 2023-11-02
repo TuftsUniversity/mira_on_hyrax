bf3dfad879ae8425307c7f7414345f764c10076f
Mike Korcynski <mkorcy@gmail.com>
2023-11-02 15:30:24 -0400

    TDLR-2507 - Remove EAD UI code and spec tests. (#100)
    * TDLR-2507 - Remove EAD UI code and spec tests.
    
    ---------
    
    Co-authored-by: Brian Goodmon <brian.goodmon@gmail.com>

--- spec/actors/hyrax/actors/ead_actor_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-# Generated via
-#  `rails generate hyrax:work Ead`
-# require 'rails_helper'
-
-# RSpec.describe Hyrax::Actors::EadActor do
-# end
--- spec/presenters/hyrax/ead_presenter_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# Generated via
-#  `rails generate hyrax:work Ead`
-require 'rails_helper'
-
-RSpec.describe Hyrax::EadPresenter do
-  it_behaves_like 'a Tufts presenter'
-end
