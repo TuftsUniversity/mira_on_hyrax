f6d6c3182f60fb22bba66aa4ed072165cff51c62
Ryan Orlando <rynorlando@gmail.com>
2024-09-04 11:03:04 -0400

    get tests running

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 r_file = Rails.root.join('config', 'redis.yml').to_s
 r_config = YAML.safe_load(File.open(r_file)).deep_symbolize_keys![Rails.env.to_sym]
+# TODO: this is depricated figure out replacement
+# Dropped support for this in hryax 5
 Redis.current = Redis.new(r_config.merge(thread_safe: true))
