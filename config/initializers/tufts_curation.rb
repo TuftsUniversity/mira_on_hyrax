b1c92688be0eaa884924809ff167aeca68dc7bf7
Mike Korcynski <mkorcy@gmail.com>
2024-10-24 10:32:56 -0400

    rubocop

--- config/initializers/tufts_curation.rb
+++ config/initializers/tufts_curation.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
 require 'tufts/curation/indexing_identifier'
 
 # config/initializers/tufts_curator.rb
 Tufts::Curation::IndexerIdentifier.configured_app_name = 'MIRA'
-
