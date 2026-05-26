4a1c946e29e7145869f84757981aa640a6c71611
Ryan Orlando <rynOrlando@gmail.com>
2026-05-26 09:41:27 -0400

    Add total items viewable

--- app/presenters/hyrax/collection_presenter.rb
+++ app/presenters/hyrax/collection_presenter.rb
@@ -9,9 +9,26 @@ module Hyrax
     # PATCH: (can be removed in 4.0, or possibly earlier)
     # Removed :size from the list because it just displays
     # 'unknown' on the interface and is to be removed in 4.0.
+    # 
+    # Also Addd :total_viewable_items
     def self.terms
-      [:total_items, :resource_type, :creator, :contributor, :keyword, :license, :publisher, :date_created, :subject,
+      [:total_items, :total_viewable_items, :resource_type, :creator, :contributor, :keyword, :license, :publisher, :date_created, :subject,
        :language, :identifier, :based_near, :related_url]
+    end
+
+    # Adding Total items visibile
+    # Because Total Items were confusing people
+    def [](key)
+      case key
+      when :size
+        size
+      when :total_items
+        total_items
+      when :total_viewable_items
+        total_viewable_items
+      else
+        solr_document.send key
+      end
     end
   end
 end
